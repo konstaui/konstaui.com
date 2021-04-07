433be0ad35fbf70c2372dbc95ac65c81f5ff28a1
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-04-07 20:02:53 +0300

    enable JIT for build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/pages/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/components/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   mode: process.env.TAILWIND_JIT ? 'jit' : undefined,
