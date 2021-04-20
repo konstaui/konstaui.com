447876bb9e7a9751d6814f92dbb817611b3c8be5
Vlad <vl.tansky@gmail.com>
2021-04-20 13:48:24 +0300

    chore: remove TAILWIND_JIT env variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     './src/components/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
-  mode: process.env.TAILWIND_JIT ? 'jit' : undefined,
+  mode: 'jit',
   theme: {
     fontFamily: {
       sans:
