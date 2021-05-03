863132fe8bf91d79a4eaf360137a9bfbbf9f9243
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-05-03 12:00:32 +0300

    fix button classes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,13 @@
 export const Button = (props) => {
-  const { component = 'a', inline, children, ...attrs } = props;
+  const { component = 'a', inline, className, children, ...attrs } = props;
   const Component = component;
   return (
     <Component
       className={`bg-primary text-xl font-bold text-white px-8 h-12 ${
         inline ? 'inline-flex' : 'flex'
-      } items-center rounded-xl cursor-pointer hover:bg-primary-light active:bg-primary-dark duration-200 active:ring-4 active:ring-primary active:ring-opacity-50`}
+      } items-center rounded-xl cursor-pointer hover:bg-primary-light active:bg-primary-dark duration-200 active:ring-4 active:ring-primary active:ring-opacity-50 ${
+        className || ''
+      }`}
       {...attrs}
     >
       {children}
