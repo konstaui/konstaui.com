bea5a0704bf625e8d5e529feb464945bb710c422
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-09-02 15:39:29 +0300

    Update build-examples.js

--- build/build-examples.js
+++ build/build-examples.js
@@ -2,17 +2,23 @@ const fs = require('fs');
 const path = require('path');
 
 const buildExamples = async () => {
-  const pagesPath = path.resolve(__dirname, '../kitchen-sink/react/pages');
+  const pagesPath = path.resolve(
+    __dirname,
+    '../public/kitchen-sink/react/pages'
+  );
   const pages = fs
     .readdirSync(pagesPath)
     .filter((page) => !page.includes('Home') && page.includes('.jsx'));
+  // BadgePage.displayName = 'BadgePage';
   pages.forEach((page) => {
     const name = page.split('.jsx')[0];
     const content = fs
       .readFileSync(path.resolve(pagesPath, page), 'utf-8')
       .replace(/\$\{/g, '\\${')
       .replace(/={`/g, '={\\`')
-      .replace(/`}/g, '\\`}');
+      .replace(/`}/g, '\\`}')
+      .replace(/([a-zA-Z]*)\.displayName = '([a-zA-Z]*)';/g, '')
+      .trim();
 
     const demoContent = `
 import { Pre } from '@/components/Pre.js';
