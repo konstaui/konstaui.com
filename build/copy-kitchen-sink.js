d1aff9af64534379dda4111c04b9bed3ccb29de3
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-10-14 14:30:11 +0300

    chore: copy Vue KS

--- build/copy-kitchen-sink.js
+++ build/copy-kitchen-sink.js
@@ -11,6 +11,12 @@ const copyKitchenSink = async () => {
   await exec(
     'cp -r ../tailwind-mobile/kitchen-sink/react/pages/* public/kitchen-sink/react/pages'
   );
+  await exec(
+    'cp -r ../tailwind-mobile/kitchen-sink/vue/dist/* public/kitchen-sink/vue/dist'
+  );
+  await exec(
+    'cp -r ../tailwind-mobile/kitchen-sink/vue/pages/* public/kitchen-sink/vue/pages'
+  );
 };
 
 copyKitchenSink();
