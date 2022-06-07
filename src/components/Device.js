bb9ba3b6010f6d775dfd9d6b2f8398880553bcb0
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-06-07 20:33:00 +0300

    0.10.3

--- src/components/Device.js
+++ src/components/Device.js
@@ -2,23 +2,23 @@
 export const Device = ({ url, float, className }) => {
   return (
     <div
-      className={`overflow-hidden w-[320px] rounded-[54px] box-content p-3 bg-black relative ${
-        float ? 'float-right ml-10 relative z-20' : ''
+      className={`relative box-content w-[320px] rounded-[54px] bg-black p-3 ${
+        float ? 'relative z-20 float-right ml-10' : ''
       } ${className || ''}`}
     >
       <iframe
         title="demo"
         src={url}
         loading="lazy"
-        className="w-[320px] h-[680px] rounded-[38px] overflow-hidden"
+        className="h-[680px] w-[320px] overflow-hidden rounded-[38px]"
       />
       {/* notch */}
       <div
-        className="absolute w-[219px] h-[23px] left-1/2 -translate-x-1/2 top-[11px] pointer-events-none z-50 bg-center bg-no-repeat"
+        className="pointer-events-none absolute left-1/2 top-[11px] z-50 h-[23px] w-[219px] -translate-x-1/2 bg-center bg-no-repeat"
         style={{ backgroundImage: 'url(/images/notch.svg)' }}
       />
       {/* bottom bar */}
-      <div className="absolute pointer-events-none z-50 w-[120px] h-1 bg-[#999] bottom-[27px] left-1/2 -translate-x-1/2 rounded-full" />
+      <div className="pointer-events-none absolute bottom-[27px] left-1/2 z-50 h-1 w-[120px] -translate-x-1/2 rounded-full bg-[#999]" />
     </div>
   );
 };
