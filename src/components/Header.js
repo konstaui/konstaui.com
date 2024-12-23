9d14c5e44e820c211878bf5247ec63f43458e753
Vladimir Kharlampidi <nolimits4web@gmail.com>
2024-12-23 13:23:49 -0500

    remove toggles banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import { Container } from './Container';
 import { ThemeSwitch } from './ThemeSwitch';
 import { GithubStats } from './GithubStats';
 import { Logo } from './Logo';
-import TogglesRibbon from './TogglesRibbon';
 
 const docsLinks = [
   { title: 'Konsta UI React', href: '/react' },
@@ -97,7 +96,6 @@ export const Header = () => {
 
   return (
     <>
-      <TogglesRibbon />
       <header className="sticky top-0 z-50 border-b-[0.5px] border-black border-opacity-10 bg-white bg-opacity-75 backdrop-blur-lg backdrop-saturate-200 dark:border-dark-light dark:bg-dark dark:bg-opacity-75 dark:backdrop-blur-lg dark:backdrop-saturate-200">
         <Container className="flex h-16 items-center justify-between ">
           <div className="flex items-center space-x-6 text-black dark:text-white">
